78b84df2c7e64bd4152ab290ecbf5cf5755174e6
tigran <Tigran Soghomonyan>
2021-10-05 14:42:30 +0400

    heroku changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/rootex"));
+app.use(express.static(__dirname + "/dist/floating-bayou-50137"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/rootex/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/floating-bayou-50137/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
